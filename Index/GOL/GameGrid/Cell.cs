16247637d4243108f17c9b866aa8e8a792ae9c13
Tobias Olsson <tobiasbjornolsson@gmail.com>
2017-01-09 16:13:16 +0100

    The grid base structure of the grid is devloping.

---
+++ Index/GOL/GameGrid/Cell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOL.GameGrid
+{
+    class Cell
+    {
+        //A Cell can be alive or dead based on this property - true = alive and false = dead
+        public Boolean IsAlive { get; set; }
+
+        // other code ...
+    }
+}
---
+++ Index/GOL/GameGrid/Row.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOL.GameGrid
+{
+    class Row
+    {
+        //List of Cells
+        public List<Cell> Cells { get; set; }
+
+        // other code ...
+    }
+}
